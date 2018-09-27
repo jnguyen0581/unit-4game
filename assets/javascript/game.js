2f91c337e3875bb758f7995e9073c6665f5f6a8a
JoyceNguyen <jnguyen0581@gmail.com>
2018-09-27 00:01:11 -0500

    working on game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,21 @@
 var crystal = {
-    crystal1
-    :{
+    crystal1:
+    {
         name: "Crystal1",
         value: 0
     },
-    crystal2
-    :{
+    crystal2:
+    {
         name: "Crystal2",
         value: 0
     },
-    crystal3
-    :{
+    crystal3:
+    {
         name: "Crystal3",
         value: 0
     },
-    crystal4
-    :{
+    crystal4:
+    {
         name: "Crystal4",
         value: 0
     }
@@ -36,7 +36,9 @@ var getRandom = function(min, max) {
 }
 var startGame = function() {
 
-    var currentScore = 0;
+    currentScore = 0;
+
+    // targetScore = Math.floor(Math.random() * (120 - 19 +1)) + 19;
 
     targetScore = getRandom(19,120);
 
@@ -48,29 +50,41 @@ var startGame = function() {
     $("#yourScore").html(currentScore);
     $("#targetScore").html(targetScore);
 
-    console.log(".......................")
-    console.log("Target Score: " + targetScore);
+    console.log("Target Score: " = targetScore);
+
     console.log("Crystal1: " + crystal.crystal1.value + " | Crystal2: " + crystal.crystal2.value | "Crystal3: " + crystal.crystal3.value | "Crystal4: " + crystal.crystal4.value);
 }
 
 var addValues = function(crystal) {
     currentScore = currentScore + crystal.value;
 
+    $("#yourScore").html(currentScore);
+   
     checkWin();
+
 
     console.log("Your Score: " + currentScore);
 
     var checkWin = function() {
-        if(currentScore + targetScore) (
+        if(currentScore > targetScore) {
             alert("Sorry. You lost!");
-            console.log("You lost!")
-        )
-        else if (currentScore = targetScore)(
+            console.log("You lost!");
+            lossCount++;
+
+            $("#lossCount").html(lossCount);
+        } 
+        
+        else if (currentScore = targetScore) {
             alert("Yay! You won!");
             console.log("You won");
-        )
+            winCount++;
+
+            $("#winCount").html(winCount);
+
+            startGame();
+        }
     }
-}
+
 // ----------------------------------------------
 
 startGame();
@@ -89,3 +103,4 @@ $("#crystal3").click(function() {
 $("#crystal4").click(function() {
     addValues(crystal.crystal4);
 });
+}
